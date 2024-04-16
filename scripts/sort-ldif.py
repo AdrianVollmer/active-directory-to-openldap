81aecd1abecb1b86ba0f8bcd7bde203d95d83c69
Adrian Vollmer <adrian.vollmer@syss.de>
2024-04-16 21:27:12 +0200

    Add sorting step of LDIF file
    LDIF files obtained with ldapsearch have no particular order. When
    importing in bulk the objects inside an LDIF file, it's important that
    parent objects are added before their child objects. By sorting the
    objects in ascending order of dn length, we ensure that that's the case.

---
+++ scripts/sort-ldif.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+
+import argparse
+import base64
+from ldif import LDIFParser, LDIFWriter
+
+from collections import OrderedDict
+
+
+class LdifParse(LDIFParser):
+
+    def parse(self):
+        """Build the index, which is a dict mapping the DN to the position in
+        the file"""
+
+        self.index = OrderedDict()
+
+        pos = 0
+        for block in self._iter_blocks():
+            first_line = block[0].partition(b'\n')[0]
+
+            if first_line.startswith(b'dn: '):
+                dn = first_line[4:].decode()
+            elif first_line.startswith(b'dn:: '):
+                dn = first_line[5:]
+                dn = base64.b64decode(dn).decode()
+            else:
+                raise RuntimeError("Parsing error at position %d" % pos)
+
+            self.index[dn] = pos
+            pos = self.byte_counter
+
+        self._input_file.seek(0)
+
+        # Sort by length of dn. This way ensures that parent objects will be
+        # inserted before child objects.
+        self.index = OrderedDict(
+            sorted(self.index.items(), key=lambda x: len(x[0]))
+        )
+
+    def __getitem__(self, dn):
+        """Access the entry in the file"""
+        try:
+            self._input_file.seek(self.index[dn])
+        except AttributeError:
+            raise RuntimeError("Index has not been built yet")
+
+        block = next(self._iter_blocks())
+        return self._parse_entry_record(block)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description="",
+    )
+
+    parser.add_argument("--src", metavar="SOURCE", help="Source ldif")
+    parser.add_argument(
+        "--dst", metavar="DESTINATION", help="Destination ldif"
+    )
+
+    args = parser.parse_args()
+
+    parser = LdifParse(open(args.src, "rb"))
+    parser.parse()
+
+    writer = LDIFWriter(open(args.dst, "wb"))
+
+    for dn in parser.index:
+        entry = parser[dn]
+        writer.unparse(dn, entry[1])
